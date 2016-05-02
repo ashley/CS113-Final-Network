8a4a5411ac4c0b5bbc0f818df96c9dee1605322f
ashley <msashleychen@gmail.com>
2016-05-01 22:14:13 -0400

    B Code

--- NetworkGUI.java
+++ NetworkGUI.java
@@ -4,19 +4,22 @@ import java.awt.event.*;
 import java.util.*;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
+import java.awt.EventQueue;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class NetworkGUI extends JFrame implements ActionListener{
 
 
-   private final int WINDOW_WIDTH = 600;
-   private final int WINDOW_HEIGHT = 800;
-   private int size = 4;
+   private final int WINDOW_WIDTH = 605;
+   private final int WINDOW_HEIGHT = 685;
    //private JButton panels[17];
-   private JButton [] panels = new JButton[size];
+   private JButton [] panels = new JButton[36];
    private JButton help, answer, reset;
    private JLabel time;
    private NetworkGameModel playGame; 
    private JLabel intro;   
+   private Timer timer = new Timer();
    
    public NetworkGUI(){
    
@@ -36,28 +39,31 @@ public class NetworkGUI extends JFrame implements ActionListener{
       
       //help label
       help = new JButton("Help");
-      help.setFont(new Font("Comic Sans MS", Font.BOLD, 14));
+      help.setFont(new Font("Helvetica", Font.BOLD, 12));
       help.setForeground(new Color(87,151,188)); //dark blue
+      help.addActionListener(this);
       pOptions.add(help);
       
       //answer label
       answer = new JButton("Answer");
-      answer.setFont(new Font("Comic Sans MS", Font.BOLD, 14));
+      answer.setFont(new Font("Helvetica", Font.BOLD, 12));
       answer.setForeground(new Color(87,151,188)); //dark blue
+      answer.addActionListener(this);
       pOptions.add(answer);
       
       //reset label
       reset = new JButton("Reset");
-      reset.setFont(new Font("Comic Sans MS", Font.BOLD, 14));
+      reset.setFont(new Font("Helvetica", Font.BOLD, 12));
       reset.setForeground(new Color(87,151,188)); //dark blue
+      reset.addActionListener(this);
       pOptions.add(reset);
    
       //time Label
       time = new JLabel("Time");
-      time.setFont(new Font("Comic Sans MS", Font.BOLD, 14));
+      time.setFont(new Font("Helvetica", Font.BOLD, 12));
       time.setForeground(new Color(238,200,239)); //blue
       pOptions.add(time);
-       
+        
          
       add (pOptions,BorderLayout.SOUTH);
    
@@ -66,15 +72,12 @@ public class NetworkGUI extends JFrame implements ActionListener{
       playGame = new NetworkGameModel();
       
       pGrid.setLayout(new GridLayout(playGame.getCols(),playGame.getRows()));
-      for(int i=0;i<size;i++){
-         String panelNum = i+1+"";
+      for(int i=0;i<36;i++){
+         playGame.setOrient(playGame.reset(i),i);
          panels[i] = new JButton (playGame.get(i));
          panels[i].setIcon(playGame.get(i));
-         panels[i].setBackground(new Color(185,228,246)); //blue
-         panels[i].setForeground(new Color(87,151,188)); //dark blue
-         panels[i].setFont(new Font ("Comic Sans MS",Font.BOLD, 32));
-         //panels[i].setBorder(customBorder);
-         //set font
+         panels[i].setBackground(new Color(185,228,246)); 
+         panels[i].setOpaque(true);//blue
          panels[i].addActionListener(this);
          pGrid.add(panels[i]);
       }
@@ -87,27 +90,58 @@ public class NetworkGUI extends JFrame implements ActionListener{
    }//public
    
    
+   
    public void actionPerformed(ActionEvent ae){
       JButton source = (JButton)ae.getSource(); 
-      if (source != reset){ //check if the button is the grid or the other buttons
+      if (source == answer){
+         Object[] options = { "OK", "CANCEL" };
+         int choice = JOptionPane.showOptionDialog(null, "Click OK to continue", "Warning",
+                                       JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE,
+                                       null, options, options[0]);
+         if(choice == 0){
+            playGame.setAnswer();
+            for(int i=0;i<36;i++){    
+               panels[i].setIcon(playGame.get(i));   
+            } 
+         }
+      }
+      else if (source == help){
+         JOptionPane.showMessageDialog(null, "Click on a cell to rotate the node.", "Help", JOptionPane.INFORMATION_MESSAGE);
+      }
+      else if (source == reset){
+         answer.addActionListener(this);
+         help.addActionListener(this);
+         playGame.resetOrient();
+         for(int i=0;i<36;i++){
+            playGame.setOrient(playGame.reset(i),i);    
+            panels[i].setIcon(playGame.get(i));
+            panels[i].setBackground(new Color(185,228,246));
+            panels[i].addActionListener(this);   
+            panels[i].setBorderPainted(true);
+            //System.out.print(playGame.getOrient(i)+" " + playGame.getAnswer(i));
+         } 
+      }
+      else{ //panel
          int i=0;
          while( source != panels[i]){
             i++; 
          }
          //panels[i].setIcon(playGame.makePath(i));
-         int num = playGame.rotateNum(playGame.getOrient(i));
-         String type = playGame.getType(i);
+         String type = playGame.getType(i);         
+         int num = playGame.rotateNum(playGame.getOrient(i),type);
          playGame.setOrient(num, i);
-         panels[i].setIcon(new ImageIcon("pieces/" + type + num + ".png"));
-         
-         if (playGame.checkStatus()){
-            for(int x=0; x<size; x++){
+         panels[i].setIcon(playGame.resizeImage(num,type));
+      } 
+      if (playGame.checkStatus()){
+            for(int x=0; x<36; x++){
                panels[x].setBackground(Color.GREEN);
+               panels[x].setBorderPainted(false);
                panels[x].removeActionListener(this);
             }
-            System.out.println(playGame.reportWinner());
-         }
-      } 
+            JOptionPane.showMessageDialog(null, "You've Won!", "Winner", JOptionPane.INFORMATION_MESSAGE);
+            answer.removeActionListener(this);
+            help.removeActionListener(this);
+      }
    }//actionPerformed
    
    
--- NetworkGameModel.java
+++ NetworkGameModel.java
@@ -3,13 +3,26 @@ import java.awt.*;
 import java.awt.event.*;
 import java.lang.*;
 import java.util.Arrays;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.util.Random;
 
 class NetworkGameModel extends GameModel{
-   public int SIZE = 4;
+   private int col = 6;
+   private int row = 6;
+   public int SIZE = 36;
+   private Random r = new Random();
    private ImageIcon [] images = new ImageIcon[SIZE];
-   private String [] panelType = {"corner", "corner", "corner","corner"};
-   private int [] panelAnswer = {2,1,3,4};
-   private int [] panelOrient = {1,1,1,1};
+   private String [] panelType = {"node", "node", "node","tee","straight","node",
+                                  "tee","corner","node","tee","tee","corner",
+                                  "straight","node","node","tee","corner","straight",
+                                  "corner","tee","straight","tee","node","corner",
+                                  "node","straight","tee","tee","tee","node",
+                                  "node", "straight","tee","node","corner","node"
+                                  };
+   private int [] panelAnswer = {2, 2, 3, 2, 1, 1, 3, 1, 3, 4, 2, 2, 2, 2, 3, 2, 1, 2, 4, 4, 1, 1, 3, 1, 3, 1, 2, 4, 2, 1, 3, 1, 4, 1, 4, 1};
+   private int [] panelOrient = new int [36];
 
    NetworkGameModel(){
       //panelOrient[0] = 2;
@@ -19,12 +32,6 @@ class NetworkGameModel extends GameModel{
    void takeTurn(int i){
    }//check
 
-   ImageIcon makePath(int i){
-      int num = rotateNum(getOrient(i));
-      String type = getType(i);
-      return (new ImageIcon("pieces/" + type + num + ".png"));
-   }
-
    boolean gameOverStatus(){
       return false;
    }//gameOverStatus
@@ -32,17 +39,34 @@ class NetworkGameModel extends GameModel{
    ImageIcon get(int i){
       int num = getOrient(i);
       String type = getType(i);
-      return(new ImageIcon("pieces/"+ type + num + ".png"));
+      return(resizeImage(num,type));
    }//ImageIcon
 
-   int rotateNum(int i){
+   ImageIcon resizeImage(int num, String type){
+      Toolkit kit = Toolkit.getDefaultToolkit();
+      Image img = kit.getImage("pieces/"+ type + num + ".png");
+      img = img.getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+      return (new ImageIcon(img));
+   }
+
+   int rotateNum(int i,String type){
       int num;
-      if(i < 4){
-         num = i+1;
+      if (!type.equals("straight")){      
+         if(i < 4){
+            num = i+1;
+         }
+         else{
+             i = 1;
+            num = 1;
+         }
       }
       else{
-          i = 1;
-         num = 1;
+         if(i == 1){
+            num = 2;
+         }
+         else{
+            num = 1;
+         }
       }
       return num;
    }
@@ -52,8 +76,9 @@ class NetworkGameModel extends GameModel{
    }
    
    boolean checkStatus(){
-       System.out.println("Orient: " + Arrays.toString(panelOrient));
-        System.out.println("Asnwer: " + Arrays.toString(panelAnswer));
+       //System.out.println(" ");
+       //System.out.println("Orient: " + Arrays.toString(panelOrient));
+       //System.out.println("Asnwer: " + Arrays.toString(panelAnswer));
       if (Arrays.toString(panelAnswer).equals(Arrays.toString(panelOrient))){
          return true;
       }
@@ -63,6 +88,10 @@ class NetworkGameModel extends GameModel{
    }
 
    int getOrient(int i){
+      if(i == -1){
+       System.out.println("Orient: " + Arrays.toString(panelOrient));
+       return 0;
+      }
       return panelOrient[i];
    }
 
@@ -75,11 +104,11 @@ class NetworkGameModel extends GameModel{
    }
    
    int getRows(){
-      return SIZE/2;
+      return row;
    }//getRows
 
    int getCols(){
-      return SIZE/2;
+      return col;
    }//getCols 
 
    void display(){}  
@@ -87,8 +116,27 @@ class NetworkGameModel extends GameModel{
    String reportWinner(){
       return "Weiner!";
    }//reportWinner
-
-
-
+   
+   void setAnswer(){
+      panelOrient = panelAnswer;
+   }
+   
+   void resetOrient(){
+      panelOrient = new int[36];
+   }
+   
+   int reset(int i){
+         int num = 0;
+         int high, low = 1;
+         if(panelType[i].equals("straight")){
+            high = 2;
+         }
+         else{
+            high = 4;
+         }
+         num = r.nextInt(high-low) + low;
+         return num;
+   }
+   
 
 }
